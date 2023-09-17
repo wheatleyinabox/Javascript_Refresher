72be6cd587ecd4ab97d539d1d7ac3502a435f36a
Valen DeLeon <valeriedeleon4521@gmail.com>
2023-09-17 16:52:51 -0700

    Super easy intro

---
+++ introduction/index.js
@@ -0,0 +1,18 @@
+console.log("Hello World!");
+console.log("Hello World! x2");
+
+window.alert("HELLO WORLD!!! :DDD");
+
+// This is a comment wowie! :O
+
+/*
+    woah
+    so
+    much
+    comment
+    but
+    it's
+    super
+    useless
+    D:
+*/

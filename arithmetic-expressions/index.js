78be8992e9e4ec125cb5cf3b4c8c85ccd07e4226
Valen DeLeon <valeriedeleon4521@gmail.com>
2023-09-17 17:53:44 -0700

    Arthimetic expressions

---
+++ arithmetic-expressions/index.js
@@ -0,0 +1,22 @@
+/*
+    arithmetic expression is a combination of...
+    operands (values, variables, etc.)
+    operators (+ - * / %)
+    that can be evaluted to a value
+    ex. y = x + 5
+
+    PEMDAS applies for operator precedence
+*/
+
+// let students = 21;
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// let extraStudents = students % 3;
+
+// console.log(students);
+// console.log(extraStudents);
+
+let result = 1 + 2 * (3 + 4);
+console.log(result);

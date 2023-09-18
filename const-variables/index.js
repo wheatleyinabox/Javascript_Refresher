89af0aea09c1ad61a3ed185e70b69471f930184a
Valen DeLeon <valeriedeleon4521@gmail.com>
2023-09-17 20:55:52 -0700

    const variables

---
+++ const-variables/index.js
@@ -0,0 +1,13 @@
+// const: a variable that can't be changed
+
+const PI = 3.14159;
+let radius, circumference;
+
+radius = window.prompt("Enter the radius of a circle");
+radius = Number(radius);
+
+//PI = 420.69
+
+circumference = 2 * PI * radius;
+
+console.log("The circumference is:", circumference);

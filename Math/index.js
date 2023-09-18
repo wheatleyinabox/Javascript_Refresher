c6a2e24faf95ee9377167925697927efc01ef6c0
Valen DeLeon <valeriedeleon4521@gmail.com>
2023-09-17 21:04:18 -0700

    Basic Math functions & consts

---
+++ Math/index.js
@@ -0,0 +1,16 @@
+let x;
+let y = 5;
+let z = 9;
+let max, min;
+
+// x = Math.round(x);
+// x = Math.floor(x);
+// x = Math.ceil(x);
+// x = Math.pow(x, 2);
+// x = Math.sqrt(x);
+// x = Math.abs(x);
+// max = Math.max(z, y, z);
+// min = Math.min(z, y, z);
+x = Math.PI;
+
+console.log(x);

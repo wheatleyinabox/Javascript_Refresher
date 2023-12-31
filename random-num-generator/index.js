549901d4aa5ee6bf0654f6958536ecb8fd4793e1
Valen DeLeon <valeriedeleon4521@gmail.com>
2023-12-30 20:44:28 -0800

    Simple rand num gen, dice roller

---
+++ random-num-generator/index.js
@@ -0,0 +1,12 @@
+let x, y ,z;
+
+document.getElementById("rollBtn").onclick = function()
+{
+    x = Math.floor(Math.random() * 6) + 1;
+    y = Math.floor(Math.random() * 6) + 1;
+    z = Math.floor(Math.random() * 6) + 1;
+
+    document.getElementById("xLabel").innerHTML = x;
+    document.getElementById("yLabel").innerHTML = y;
+    document.getElementById("zLabel").innerHTML = z;
+};

d91d06f4b34c87bf28a0db732cedc615d947c8ca
Valen DeLeon <valeriedeleon4521@gmail.com>
2024-01-03 13:42:58 -0800

    arrow methods (so cool so cool)

---
+++ arrow-functions/index.js
@@ -0,0 +1,33 @@
+// arrow functions -> a concise way to write function expressions
+//                    good for simple functions that you use only once
+//                    (parameters) -> some code
+
+
+
+// EXAMPLE 1
+const hello = (name, age) => {console.log(`Hello ${name}!`)
+                              console.log(`You are ${age} years old!`)};
+
+hello("WheatleyInABox", "20");
+
+
+
+// EXAMPLE 2
+setTimeout(() => console.log("Hello!"), 3000);
+
+
+
+// EXAMPLE 3
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const squares = numbers.map((element) => Math.pow(element, 2));
+const cubes = numbers.map((element) => Math.pow(element, 3));
+const evenNums = numbers.filter((element) => element % 2 === 0);
+const oddNums = numbers.filter((element) => element % 3 !== 0);
+const total = numbers.reduce((accumlator, element) => accumlator + element);
+
+console.log(squares);
+console.log(cubes);
+console.log(evenNums);
+console.log(oddNums);
+console.log(total);

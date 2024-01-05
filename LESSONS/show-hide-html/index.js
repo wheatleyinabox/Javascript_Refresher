2749406064b97bbd288855390dc60adb8543d8a6
Valen DeLeon <valeriedeleon4521@gmail.com>
2024-01-04 22:04:12 -0800

    showing and hiding html elements

---
+++ LESSONS/show-hide-html/index.js
@@ -0,0 +1,16 @@
+const myButton = document.getElementById("myButton");
+const myImg = document.getElementById("myImg");
+
+myButton.addEventListener("click", (event) => 
+{
+    if (myImg.style.visibility === "hidden")
+    {
+        myImg.style.visibility = "visible";
+        myButton.textContent = "Hide";
+    }
+    else
+    {
+        myImg.style.visibility = "hidden";
+        myButton.textContent = "Show";
+    }
+});
